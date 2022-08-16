e46581eb74dcde13fbb5bec08ea6cc04d621d9bd
Yunus Durmus <redbullrally4@gmail.com>
2022-08-16 22:56:55 +0300

    kendin yap denemesi2

---
+++ src/Day2GitGithub.java
@@ -0,0 +1,13 @@
+public class Day2GitGithub {
+    public static void main(String[] args) {
+        String str1="yunus";
+        System.out.println("str1 = " + str1);
+        //ekrana yunus yazdirir
+        String str="Cemile";
+        System.out.println("str = " + str);
+
+
+        //KENDIN YAP DENEMESI
+        //rtgdfgsgs
+    }
+}
